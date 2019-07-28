199c57868f3a8a69d5a6d36ff4fa4262bf838873
Glauber M. Carvalho <gmauch.carvalho@gmail.com>
2019-07-28 18:43:34 -0300

    desafio 005 de python do curso em video

---
+++ desafio_005.py
@@ -0,0 +1,7 @@
+'''
+Faça um programa que leia um número inteiro e mostre na tela o seu
+sucessor e o seu antecessor.
+'''
+
+num = int(input('Digite um número: '))
+print(f'O antecessor do número que você digitou é {num-1}, o número que você digitou é {num} e o sucessor dele é {num+1}.')
