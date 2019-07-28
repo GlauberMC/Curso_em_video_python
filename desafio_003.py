20bcdf9dd9694625e273e5460426c8c86ba46d22
Glauber M. Carvalho <gmauch.carvalho@gmail.com>
2019-07-28 17:04:31 -0300

    desafio 003 de python do curso em video

---
+++ desafio_003.py
@@ -0,0 +1,10 @@
+'''
+Crie um programa que leia dois números e mostre a soma entre eles.
+'''
+
+num1 = int(input('Digite um número inteiro: '))
+num2 = int(input('Digite um outro número inteiro: '))
+
+soma = num1 + num2
+
+print(f'A soma de {num1} + {num2} é igual a {soma}.')
